f96a5b4949c7ab2e25b10c64a5945a28d5b59d25
nimol <limnimol.dev@gmail.com>
2026-01-11 10:01:20 +0700

    Done Header

--- wp-content/themes/my-theme/front-page.php
+++ wp-content/themes/my-theme/front-page.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
-
+<br><br><br><br>
 <?php get_footer(); ?>
 
---
+++ wp-content/themes/my-theme/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="box-menu">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" alt="<?php bloginfo('hospital'); ?>">
+            </div>
+            
+            <div class="container-header">
+
+                <div class="booking">
+                    <button class="btn-book">
+                            <a href="#">
+                                <i class="far fa-calendar-check"></i>
+                            </a>
+                            <a href="#">Book an appointment</a>
+                    </button>
+                </div>
+
+                <div class="sign-in">
+                    <a href="#">
+                        <i class="fas fa-user-md"></i>                        
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-search"></i>
+                    </a>
+                </div>
+                
+            </div>
+
+            </div> 
+
+        </div>
+         
+        
+        <nav>
+            
+            <?php wp_nav_menu(['theme_location'=>'primary', 
+            'menu_class'     => 'menu',
+            ]); ?>
+              
+        </nav> 
+
+        
+            
+    
+    </header>
+
+   <main>
