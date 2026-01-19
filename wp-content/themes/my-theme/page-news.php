4a4eaa3ee4c20027841bb73ce294dbe0bdb9c7c9
nimol <limnimol.dev@gmail.com>
2026-01-19 15:45:23 +0700

    done media- news-page

---
+++ wp-content/themes/my-theme/page-news.php
@@ -0,0 +1,115 @@
+<?php get_header(); ?>
+
+    <div class="page-header3">
+        <p class="breadcrumb3">HOME / MEDIA / NEWS</p>
+        <h1>NEWS</h1>
+    </div>
+
+<section class="search-section">
+    
+  <div class="search-inner">
+    <input type="text" placeholder="Search news">
+
+    <select>
+      <option>Year</option>
+      <option>2026</option>
+      <option>2025</option>
+      <option>2024</option>
+    </select>
+
+      <button>Search</button>
+
+  </div>
+
+</section>
+
+<section class="content">
+    
+  <div class="featured">
+    
+      
+
+    <div class="featured-image">
+      
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/news1.png" alt="Hero Banner">
+        
+        </div>
+      
+      <div class="featured-text">
+        
+        <h2>Remarkable surgery saves a child's hand after complete tissue and vessel damage</h2>
+        <p>
+          Upon the child's arrival at the emergency department, urgent first aid was administered,
+          and an integrated medical team assessed the condition in detail...
+        </p>
+        <a href="#">Read Full Story →</a>
+
+      </div>
+    </div>
+
+    <div class="cards">
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/new2.png" alt="Hero Banner">
+
+        <div class="card-body">
+          <span>News</span>
+          <h3>From a Misdiagnosis to the Birth of Triplets</h3>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/new1.png" alt="Hero Banner">
+        <div class="card-body">
+
+          <span>News</span>
+          <h3>Successful Scoliosis Surgery for 14-Year-Old Girl</h3>
+          <a href="#">Read More</a>
+
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/new4.png" alt="Hero Banner">
+        <div class="card-body">
+          <span>News</span>
+          <h3>New MRI Machine Installed at Hospital</h3>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/news1.png" alt="Hero Banner">
+        <div class="card-body">
+          <span>News</span>
+          <h3>Health Awareness Campaign Launched</h3>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/new2.png" alt="Hero Banner">
+        <div class="card-body">
+          <span>News</span>
+          <h3>Advanced Surgical Unit Opens</h3>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/new2.png" alt="Hero Banner">
+        <div class="card-body">
+          <span>News</span>
+          <h3>Advanced Surgical Unit Opens</h3>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+    </div>
+
+</section>
+
+
+<br> <br> <br>
+<?php get_footer(); ?>
